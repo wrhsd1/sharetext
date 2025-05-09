75648aa4057a8f5ada57b7d9d3e4399441beb73b
wrhsd1 <wangrenhuan@gmail.com>
2025-05-09 15:58:30 +0800

    初始提交

--- android/app/src/main/kotlin/com/example/sharetext/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sharetext/MainActivity.kt
@@ -1,7 +1,22 @@
 package com.example.sharetext
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    // 使用Android嵌入V2版本，不需要额外代码
+    // 我们可以直接使用缓存引擎，或者在这里配置一个新引擎
+    private val ENGINE_ID = "share_text_engine"
+    
+    // 使用我们在Application类中预热的引擎
+    override fun getCachedEngineId(): String? {
+        return ENGINE_ID
+    }
+    
+    // 如果不使用缓存引擎，则确保配置新引擎
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // 通过调用GeneratedPluginRegistrant注册所有的插件
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
--- android/app/src/main/kotlin/com/example/sharetext/ShareTextApp.java
+++ android/app/src/main/kotlin/com/example/sharetext/ShareTextApp.java
@@ -1,11 +1,26 @@
 package com.example.sharetext;
 
-import io.flutter.app.FlutterApplication;
+import android.app.Application;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-// 继承FlutterApplication以支持Android嵌入V2
-public class ShareTextApp extends FlutterApplication {
+// 使用标准Android Application类而不是旧的FlutterApplication
+public class ShareTextApp extends Application {
+    private static final String ENGINE_ID = "share_text_engine";
+    
     @Override
     public void onCreate() {
         super.onCreate();
+        // 预热一个FlutterEngine以避免冷启动延迟
+        FlutterEngine flutterEngine = new FlutterEngine(this);
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        );
+        // 注册所有插件
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        // 缓存引擎以便MainActivity可以使用它
+        FlutterEngineCache.getInstance().put(ENGINE_ID, flutterEngine);
     }
 }
